6872c1e52ae9a2cfbc8e61e58a768d2d8917328f
solutionsSlayer <ddoussain@gmail>
2019-05-23 14:14:40 +0200

    UserRegister

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -1,10 +1,7 @@
 <?php
-
 namespace App\Entity;
-
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
-
 /**
  * Class User
  * @ORM\Entity
@@ -18,11 +15,98 @@ class User extends BaseUser
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
-
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $fname;
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $lname;
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $phone;
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $city;
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    protected $created_at;
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    protected $updated_at;
     public function __construct()
     {
+        $this->created_at = new \DateTime('now');
+        $this->updated_at = new \DateTime('now');
         parent::__construct();
     }
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+    public function getFname(): ?string
+    {
+        return $this->fname;
+    }
+    public function setFname(string $fname): self
+    {
+        $this->fname = $fname;
+        return $this;
+    }
+    public function getLname(): ?string
+    {
+        return $this->lname;
+    }
+    public function setLname(string $lname): self
+    {
+        $this->lname = $lname;
+        return $this;
+    }
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+    public function setPhone(string $phone): self
+    {
+        $this->phone = $phone;
+        return $this;
+    }
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+    public function setCity(string $city): self
+    {
+        $this->city = $city;
+        return $this;
+    }
 
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->created_at;
+    }
 
+    public function setCreatedAt(\DateTimeInterface $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updated_at): self
+    {
+        $this->updated_at = $updated_at;
+
+        return $this;
+    }
 }
